0fee67501e759c6112322fd2e11f4855803c0023
Palmério <palmerio_julio@hotmail.com>
2024-07-12 01:25:05 -0300

    Iniciando criação dos componente leitor e o formulário do leitor.

---
+++ biblioteca-frontend/src/app/modules/leitor-form/leitor-form.component.ts
@@ -0,0 +1,105 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Leitor } from 'src/app/models/leitor-model';
+import { LeitorService } from 'src/app/services/leitor.service';
+
+@Component({
+  selector: 'app-leitor-form',
+  templateUrl: './leitor-form.component.html',
+  styleUrls: ['./leitor-form.component.scss']
+})
+export class LeitorFormComponent implements OnInit {
+
+  formLeitor!: FormGroup;// Declaração do formulário do leitor
+  durationInSeconds = 5;// Duração da exibição do snack bar em segundos
+  btn: string = "Salvar"// Texto do botão
+  title: string = "Adicionar Leitor"// Título do formulário
+
+  constructor(
+    private LeitorService: LeitorService, // Serviço para manipulação de leitores
+    public dialogRef: MatDialogRef<LeitorFormComponent>, // Referência ao diálogo atual
+    private snackBar: MatSnackBar, // Serviço de snack bar para exibir mensagens
+    @Inject(MAT_DIALOG_DATA) public LeitorEdit: Leitor // Dados injetados ao abrir o diálogo, para edição
+  ) { }
+
+  // Método executado quando o componente é inicializado
+  ngOnInit(): void {
+    this.createForm(new Leitor());// Criação do formulário com um novo leitor
+  }
+
+  // Método para criar o formulário do leitor
+  createForm(leitor: Leitor) {
+    // Criação do grupo de controles do formulário com validações
+    this.formLeitor = new FormGroup({
+      id: new FormControl(leitor.id, Validators.required),
+      nome: new FormControl(leitor.nome, Validators.required),
+      cpf: new FormControl(leitor.cpf, Validators.required),
+      rg: new FormControl(leitor.rg),
+      dataNascimento: new FormControl(leitor.dataNascimento, Validators.required),
+      telefone: new FormControl(leitor.telefone, Validators.required),
+      profissao: new FormControl(leitor.profissao),
+      escola: new FormControl(leitor.escola),
+      serie: new FormControl(leitor.serie),
+      curso: new FormControl(leitor.curso),
+      turno: new FormControl(leitor.turno)
+    });
+
+    // Se há um Leitor para editar, preenche os campos do formulário com os dados do leitor
+    if (this.LeitorEdit) {
+      this.btn = "Editar",
+        this.title = "Editar leitor",
+        this.formLeitor.controls['id'].setValue(this.LeitorEdit.id),
+        this.formLeitor.controls['nome'].setValue(this.LeitorEdit.nome),
+        this.formLeitor.controls['cpf'].setValue(this.LeitorEdit.cpf),
+        this.formLeitor.controls['rg'].setValue(this.LeitorEdit.rg),
+        this.formLeitor.controls['dataNascimento'].setValue(this.LeitorEdit.dataNascimento),
+        this.formLeitor.controls['telefone'].setValue(this.LeitorEdit.telefone),
+        this.formLeitor.controls['escola'].setValue(this.LeitorEdit.escola),
+        this.formLeitor.controls['serie'].setValue(this.LeitorEdit.serie),
+        this.formLeitor.controls['curso'].setValue(this.LeitorEdit.curso),
+        this.formLeitor.controls['turno'].setValue(this.LeitorEdit.turno)
+
+    }
+  }
+
+  // Método chamado ao submeter o formulário
+  onSubmit() {
+    // Se não for edição, salva um novo leitor
+    if (this.btn != "Editar") {
+      this.LeitorService.salvarLeitor(this.formLeitor.value).subscribe((res: any) => {
+        if (res != null) {
+          this.open('Leitor salvo com sucesso!', 'X');
+        } else {
+          this.open('Erro ao salvar o leitor!', 'X');
+        }
+        this.fecharModal();
+      })
+      this.formLeitor.reset(new Leitor());// Reseta o formulário para um novo leitor
+
+    // Se for edição, atualiza o leitor existente
+    } else {
+      this.LeitorService.editarLeitor(this.formLeitor.value).subscribe((res: any) => {
+        if (res != null) {
+          this.open('Leitor alterado com sucesso!', 'X');
+        } else {
+          this.open('Erro ao alterar o leitor!', 'X');
+        }
+        this.fecharModal();
+      })
+    }
+  }
+
+  // Método para exibir uma mensagem usando o snack bar
+  open(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: this.durationInSeconds * 1000,
+    });
+  }
+
+  // Método para fechar o diálogo
+  public fecharModal() {
+    this.dialogRef.close();
+  }
+}
--- biblioteca-frontend/src/app/modules/leitor/leitor.component.ts
+++ biblioteca-frontend/src/app/modules/leitor/leitor.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
+import { Leitor } from 'src/app/models/leitor-model';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
+import { LeitorService } from 'src/app/services/leitor.service';
+import { LeitorFormComponent } from '../leitor-form/leitor-form.component';
 
 @Component({
   selector: 'app-leitor',
@@ -7,9 +15,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeitorComponent implements OnInit {
 
-  constructor() { }
+  // Variáveis para construção da lista de dos leitor.
+  @ViewChild(MatPaginator) paginator!: MatPaginator; // Paginator para a tabela
+  displayedColumns: string[] = ['id', 'nome', 'telefone', 'profissao', 'escola', 'editar', 'excluir'] // Colunas exibidas na tabela
+  dataSource!: MatTableDataSource<Leitor>; // Fonte de dados para a tabela
+  leitor!: Leitor; // Objeto do tipo Leitor
+  durationInSeconds = 5; // Duração para o snackbar
+
+  constructor(
+    private leitorService: LeitorService, // Serviço para comunicação com o backend
+    private dialog: MatDialog, // Serviço para diálogos
+    private snackBar: MatSnackBar // Serviço para snackbars
+  ) { }
 
   ngOnInit(): void {
+    this.listarLeitores(); // Carrega a lista de leitores ao iniciar o componente
   }
 
+  // Método para listar leitores
+  listarLeitores() {
+    this.leitorService.listarLeitores().subscribe((res: any) => {
+      this.dataSource = new MatTableDataSource(res);
+      this.dataSource.paginator = this.paginator; // Associa o paginator à tabela
+    });
+  }
+
+  // Método para deletar um leitor com confirmação em diálogo
+  deletarLeitor(leitor: any) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '400px',
+      data: {
+        title: 'Confirmar Exclusão',
+        message: 'Deseja realmente excluir o leitor?',
+        titulo: `${leitor.titulo}`
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.leitorService.deletarLeitor(leitor.id).subscribe((res: any) => {
+          this.listarLeitores(); // Atualiza a lista de leitores após a exclusão
+          this.open('Leitor excluído com sucesso!', 'Fechar');
+        }, (error) => {
+          alert(error.error.text);
+          this.listarLeitores();
+        });
+      }
+    });
+  }
+
+  // Método para abrir o formulário de edição de leitor
+  editarLeitor(leitor: any) {
+    const dialogRef = this.dialog.open(LeitorFormComponent, {
+      width: '800px',
+      data: leitor // Passa o leitor atual como dado para o diálogo
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.listarLeitores(); // Atualiza a lista de leitores após o fechamento do diálogo
+    });
+  }
+
+  // Método para abrir o formulário de criação de leitor
+  openDialog(leitor: Leitor) {
+    const dialogRef = this.dialog.open(LeitorFormComponent, {
+      width: '800px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.listarLeitores(); // Atualiza a lista de leitores após o fechamento do diálogo
+    });
+  }
+
+  // Método para abrir um snackbar com uma mensagem
+  open(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: this.durationInSeconds * 1000,
+    });
+  }
+
+  // Método para aplicar filtro na tabela
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase(); // Aplica o filtro na tabela
+  }
 }
