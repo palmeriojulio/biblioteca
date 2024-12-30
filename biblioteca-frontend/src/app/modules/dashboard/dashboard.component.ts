fefd1010f5d36629ff4a1f6e3bcec0137749ad87
Palmério <palmerio_julio@hotmail.com>
2024-12-29 23:53:40 -0300

    Adicionando mais um gráfico no Dashboard

--- biblioteca-frontend/src/app/modules/dashboard/dashboard.component.ts
+++ biblioteca-frontend/src/app/modules/dashboard/dashboard.component.ts
@@ -10,7 +10,7 @@ export class DashboardComponent implements OnInit {
 
   cards = [
     { title: 'Livros', value: '786', icon: 'book'},
-    { title: 'Leitores', value: '150', icon: '🧒'},
+    { title: 'Leitores', value: '150', icon: 'groups'},
     { title: 'Emprestimos', value: '15', icon: 'book'},
     { title: 'Entregas de Hoje', value: '8', icon: 'today'},
     { title: 'Entregas em Atraso', value: '3', icon: 'schedule'}
@@ -23,6 +23,7 @@ export class DashboardComponent implements OnInit {
     Chart.register(...registerables);
 
     const ctx = (document.getElementById('booksChart') as HTMLCanvasElement).getContext('2d');
+    const doughnutChart = (document.getElementById('doughnutChart') as HTMLCanvasElement).getContext('2d');
 
     if (ctx) { // Verifica se ctx não é null
       new Chart(ctx, {
@@ -30,10 +31,22 @@ export class DashboardComponent implements OnInit {
         data: {
           labels: ['O Código da Inteligência', 'Pai Rico Pai Pobre', 'Como Fazer Amigos...', 'Macunaíma', 'O Senhor dos Aneis'],
           datasets: [{
-            label: '5 Livros mais emprestados',
+            label: 'Livros mais emprestados',
             data: [30, 25, 20, 10, 5],
-            backgroundColor: 'rgba(33, 149, 243, 0.47)',
-            borderColor: 'rgba(33, 150, 243, 1)',
+            backgroundColor: [
+              'rgba(255, 205, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+              'rgb(255, 205, 86)',
+              'rgb(75, 192, 192)',
+              'rgb(54, 162, 235)',
+              'rgb(153, 102, 255)',
+              'rgb(201, 203, 207)'
+            ],
             borderWidth: 1
           }]
         },
@@ -47,6 +60,30 @@ export class DashboardComponent implements OnInit {
               beginAtZero: true
             }
           }
+        }
+      });
+    }
+
+    if (doughnutChart) {
+      new Chart(doughnutChart, {
+        type: 'doughnut',
+        data: {
+          labels: ['Livros com atraso'],
+          datasets: [{
+            label: 'Livros em atraso',
+            data: [30, 25, 20, 10, 5],
+            backgroundColor: [
+              'rgba(255, 200, 70, 0.76)',
+              'rgba(75, 192, 192, 0.75)',
+              'rgba(54, 163, 235, 0.83)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(201, 203, 207, 0.2)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          responsive: true,
         }
       });
     }
